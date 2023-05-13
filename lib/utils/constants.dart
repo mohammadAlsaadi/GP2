621982aec08a2e16f81e33f106b9241688c51517
Mohammad Bader <mnb.99@hotmail.com>
2023-05-14 02:10:38 +0300

    Added constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//Background Colors
+const mBackgroundColor = Color(0xFFF5FAFF);
+
+//Main color
+const mRedColor = Color(0xFFFF5050);
+
+//Box color
+final shadowColor = const Color(0xFFB7B7B7).withOpacity(.15);
+const boxColor = Color(0xFF91A2BA);
+
+//
+final accent = const Color.fromARGB(255, 237, 53, 53).withOpacity(.1);
+
+//Text
+const suggested = 'Suggested';
+const category = 'Category';
+const seeAll = 'See All';
